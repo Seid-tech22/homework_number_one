ecddbb5130dc8a46331c134d9c48c47ef5f1c67e
Seid-tech22 <seidkakayev@gmail.com>
2022-11-22 21:10:58 +0300

    Fixed screen ui

--- lib/main.dart
+++ lib/main.dart
@@ -26,25 +26,24 @@ class __SeidAppState extends State<_SeidApp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: false,
-      body: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 110, horizontal: 20),
+      body: Center(
         child: Column(
           children: [
+            const SizedBox(height: 90),
             Container(
               padding: const EdgeInsets.all(20),
               decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(20),
                   color: const Color.fromARGB(255, 3, 26, 29)),
               width: MediaQuery.of(context).size.width * 0.90,
-              height: MediaQuery.of(context).size.height * 0.20,
+              height: MediaQuery.of(context).size.height * 0.18,
               child: const Text(
                 ' До 17:00 сегодня \n \n Закончить\n презентацию проекта\n DontSleep',
                 style:
                     TextStyle(fontWeight: FontWeight.w800, color: Colors.white),
               ),
             ),
-            const SizedBox(height: 15),
+            const SizedBox(height: 10),
             Container(
               padding: const EdgeInsets.all(20),
               decoration: BoxDecoration(
@@ -52,14 +51,14 @@ class __SeidAppState extends State<_SeidApp> {
                 color: Colors.greenAccent.shade700,
               ),
               width: MediaQuery.of(context).size.width * 0.90,
-              height: MediaQuery.of(context).size.height * 0.20,
+              height: MediaQuery.of(context).size.height * 0.18,
               child: const Text(
                 ' До 23:59 сегодня\n\n Отправить\n самостоятельную\n работу по физике',
                 style:
                     TextStyle(fontWeight: FontWeight.w800, color: Colors.white),
               ),
             ),
-            const SizedBox(height: 15),
+            const SizedBox(height: 10),
             Container(
               padding: const EdgeInsets.all(20),
               decoration: BoxDecoration(
@@ -67,57 +66,56 @@ class __SeidAppState extends State<_SeidApp> {
                 color: const Color.fromARGB(214, 126, 101, 1),
               ),
               width: MediaQuery.of(context).size.width * 0.90,
-              height: MediaQuery.of(context).size.height * 0.20,
+              height: MediaQuery.of(context).size.height * 0.18,
               child: const Text(
                 ' До 12:30 завтра\n \n Заполнить документы\n на ИП',
                 style:
                     TextStyle(fontWeight: FontWeight.w800, color: Colors.white),
               ),
             ),
-            const SizedBox(height: 80),
-            SingleChildScrollView(
-              child: Expanded(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Container(
-                      padding: const EdgeInsets.symmetric(
-                          vertical: 27, horizontal: 35),
-                      width: MediaQuery.of(context).size.width * 0.40,
-                      height: MediaQuery.of(context).size.height * 0.10,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20),
-                          color: const Color.fromARGB(255, 38, 29, 29)),
-                      child: const Text(
-                        'Добавить',
-                        style: TextStyle(
-                          fontWeight: FontWeight.w900,
-                          color: Colors.white,
-                          fontSize: 17,
-                        ),
+            const Spacer(),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: MediaQuery.of(context).size.width * 0.40,
+                  height: MediaQuery.of(context).size.height * 0.10,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: const Color.fromARGB(255, 38, 29, 29)),
+                  child: const Center(
+                    child: Text(
+                      'Добавить',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w900,
+                        color: Colors.white,
+                        fontSize: 17,
                       ),
                     ),
-                    Container(
-                      padding: const EdgeInsets.symmetric(
-                          vertical: 27, horizontal: 43),
-                      width: MediaQuery.of(context).size.width * 0.40,
-                      height: MediaQuery.of(context).size.height * 0.10,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20),
-                          color: const Color.fromARGB(255, 38, 29, 29)),
-                      child: const Text(
-                        'Удалить',
-                        style: TextStyle(
-                          fontWeight: FontWeight.w800,
-                          color: Colors.white,
-                          fontSize: 17,
-                        ),
-                      ),
-                    ),
-                  ],
+                  ),
                 ),
-              ),
-            )
+                const SizedBox(width: 35),
+                Container(
+                  width: MediaQuery.of(context).size.width * 0.40,
+                  height: MediaQuery.of(context).size.height * 0.10,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: const Color.fromARGB(255, 38, 29, 29)),
+                  child: const Center(
+                    child: Text(
+                      'Удалить',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w800,
+                        color: Colors.white,
+                        fontSize: 17,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 30),
           ],
         ),
       ),
